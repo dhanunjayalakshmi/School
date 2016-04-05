7052e624ddcc2461e360ca78716f84e55f305a48
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2016-04-05 11:04:59 +0530

    Create API for all restful actions of Students Controller

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -32,6 +32,15 @@ module Api
 
       def update
         @student.name = params[:name] || @student.name
+        @student.roll_number = params[:roll_number] || @student.roll_number
+        @student.fathers_name = params[:fathers_name] || @student.fathers_name
+        @student.gender = params[:gender] || @student.gender
+        @student.email = params[:email] || @student.email
+        @student.dob = params[:dob] || @student.dob
+        @student.phone = params[:phone] || @student.phone
+        @student.address = params[:address] || @student.address
+        @student.house_id = params[:house_id] || @student.house_id
+        @student.section_id = params[:section_id] || @student.section_id
         if @student.save
           head :serveraccepted, location: api_klass_section_student_url(@klass, @section, @student)
         else
@@ -40,7 +49,7 @@ module Api
       end
 
       def destroy
-        if params[:roll_number] == @student.roll_number
+        if params[:roll_number].to_i == @student.roll_number.to_i
           @student.destroy
           head :ok
         else
